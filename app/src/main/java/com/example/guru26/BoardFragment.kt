a8bbf5bb4fbb8db2d2a61828a4117528a5f110e9
soyoung1832 <jih32002@naver.com>
2023-07-24 15:02:59 +0900

    -전시회 등록 페이지 버튼 추가  (키보드 ux 개선)

--- app/src/main/java/com/example/guru26/BoardFragment.kt
+++ app/src/main/java/com/example/guru26/BoardFragment.kt
@@ -1,10 +1,13 @@
 package com.example.guru26
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+import com.google.android.material.internal.ContextUtils.getActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +19,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [BoardFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class BoardFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
