302154cfb73fcae8cf8effed1fba9a09b4806874
soyoung1832 <jih32002@naver.com>
2023-07-26 01:10:24 +0900

    -탭바 수정 (마이페이지)

--- app/src/main/java/com/example/guru26/WorteFragment.kt
+++ app/src/main/java/com/example/guru26/WorteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,11 +23,18 @@ class WorteFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var adapter:PosterAdapter
+    private lateinit var rv_poster: RecyclerView
+    private lateinit var posterList:ArrayList<Poster>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+
         }
     }
 
@@ -56,5 +65,26 @@ class WorteFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        rv_poster = view.findViewById(R.id.rv_poster)
+        rv_poster.layoutManager=layoutManager
+        rv_poster.setHasFixedSize(true)
+        rv_poster.adapter=PosterAdapter(posterList)
+    }
+
+
+    private fun dataInitialize(){
+
+        posterList = arrayListOf(
+            Poster(R.drawable.rectangle_657, "두 가지 색 : 이색", 20230720 - 31, "50주년 기념관 2층"),
+            Poster(R.drawable.poster, "시각디자인 졸업전시회 '금'", 20230702, "이음 갤러리"),
+
+        )
+
     }
 }
