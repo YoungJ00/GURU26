6c981f0ec08573b7be573bcb22f34e7c09feb655
soyoung1832 <jih32002@naver.com>
2023-07-29 16:34:13 +0900

    파이어베이스

--- app/src/main/java/com/example/guru26/ContentDTO.kt
+++ app/src/main/java/com/example/guru26/ContentDTO.kt
@@ -14,6 +14,6 @@ data class ContentDTO(var explain : String?=null,
                       var exhTime : String?= null,
                       var exhLink : String? = null,
                       var timeStamp : Long? = null,
-                      var likeCount: Int =0,
-                      var likes : MutableMap<String,Boolean> = HashMap()){
+                      var favoriteCount: Int =0,
+                      var favorites : MutableMap<String,Boolean> = HashMap()){
 }
--- app/src/main/java/com/example/guru26/HomeFragment.kt
+++ app/src/main/java/com/example/guru26/HomeFragment.kt
@@ -5,52 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions
-
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     var firestore : FirebaseFirestore? = null
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_home, container, false)
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, container, false)
         firestore = FirebaseFirestore.getInstance()
-
 
         view.findViewById<RecyclerView>(R.id.rv_poster).adapter = PosterAdapter()
         view.findViewById<RecyclerView>(R.id.rv_poster).layoutManager = LinearLayoutManager(activity)
 
         return view
     }
-
 
     inner class PosterAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs : ArrayList<ContentDTO > = arrayListOf()
@@ -74,7 +50,7 @@ class HomeFragment : Fragment() {
             return CustomViewHolder(view)
         }
 
-        inner class CustomViewHolder (view : View) : RecyclerView.ViewHolder(view)
+        inner class CustomViewHolder (view : View) : RecyclerView.ViewHolder(view){}
 
 
         override fun getItemCount(): Int {
@@ -95,29 +71,5 @@ class HomeFragment : Fragment() {
             Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.findViewById(R.id.iv_poster))
 
         }
-
-
-
-    }
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
